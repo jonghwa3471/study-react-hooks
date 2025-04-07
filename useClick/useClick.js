2bc81faeb90ee521d67325648a954db5127d6533
Jonghwa <whdghk3471@gmail.com>
2025-04-07 16:56:44 +0900

    #2.2 useClick

---
+++ useClick/useClick.js
@@ -0,0 +1,18 @@
+const useClick = (onClick) => {
+  const ref = useRef();
+  useEffect(() => {
+    const element = ref.current;
+    if (typeof onClick !== "function") {
+      return;
+    }
+    if (element) {
+      element.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element) {
+        element.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return ref;
+};
